d29a032196f25c3337cf769765d17011a4537c30
Matt-Boeren <matt.boeren@hotmail.com>
2025-07-12 00:13:46 +0200

    moved functions into different file

--- lua/nvimEmmet/init.lua
+++ lua/nvimEmmet/init.lua
@@ -1,109 +1,19 @@
 local M = {}
----@param trigger string
----@param indent integer
-local tagTrigger = function(trigger, indent)
-    local tag = ""
-    if indent ~= 0 then
 
-        local ind = string.rep("\t", indent)
+local fn = require("nvimEmmet.functions")
 
-        tag = ind .."<" .. trigger .. ">\n".. ind .."\t\n" .. ind .. "</" ..trigger.. ">"
-    else
-        tag = "<" .. trigger .. ">\n\t\n</" ..trigger.. ">"
-    end
-
-    return tag
-end
-
----@param command string
-local cmdToArray = function (command)
-    local strArray = {}
-    local lastIndex = 1
-    for i = 1, #command do
-        local c = command:sub(i,i)
-        if c == ">" or c == "*" then
-            table.insert(strArray, command:sub(lastIndex, i-1))
-            table.insert(strArray, c)
-            lastIndex = i+1
-        elseif i == #command then --if last character
-            table.insert(strArray, command:sub(lastIndex, i))
-        end
-
-    end
-    return strArray
-end
-
----@param str string
----@param pat string
----@param rep string
-local strReplace = function (str, pat, rep)
-    local index = #str
-    while index > #pat do
-        local part = str:sub(index-#pat, index - 1)
-        if part == pat then
-            str = string.sub(str, 1, index-#pat - 1) .. rep .. string.sub(str, index, #str)
-            index = 0
-        end
-        index = index -1
-    end
-    return str
-end
 
 local emmet = function (opts)
     local command = opts.args
-    local resultArray = {}
 
-    local strArray = cmdToArray(command)
+    local strArray = fn.cmdToArray(command)
 
  --   vim.fn.setreg('r', strArray)
  --   vim.cmd('put! r')
-    local i = 1
-
-    local indent = 0
-    while i <= #strArray do
-        if tonumber(strArray[i]) and strArray[i+1] == "*" then
-            local number = tonumber(strArray[i])
-            local tag = ""
-            if i > 1 and strArray[i-1] == ">" then
-                indent = indent + 1
-            end
-            tag = tagTrigger(strArray[i+2], indent)
-
-            local res = tag
-            for j = 2, number, 1 do
-                res = res .. "\n" .. tag
-            end
-            table.insert(resultArray, res)
-            i = i + 3
-        elseif strArray[i] == ">" then
-            table.insert(resultArray, strArray[i])
-            i = i + 1
-        else
-
-            if i > 1 and strArray[i-1] == ">" then
-                indent = indent + 1
-            end
-            table.insert(resultArray, tagTrigger(strArray[i], indent))
-            i = i + 1
-        end
-    end
-
+    local resultArray = fn.getResultArray(strArray)
 --    vim.fn.setreg('r', resultArray)
 --    vim.cmd('put! r')
-    local result = ""
-    local j = 1
-    indent = 1
-    while j <= #resultArray do
-        if resultArray[j] == ">" then
-            local tabs = string.rep("\t", indent)
-            result = strReplace(result, tabs, resultArray[j+1])
-            indent = indent + 1
-            j = j + 2
-        else
-            result = result .. resultArray[j]
-            j = j + 1
-        end
-    end
+    local result = fn.getResult(resultArray)
     vim.fn.setreg('r', result)
     vim.cmd('put! r')
 end
